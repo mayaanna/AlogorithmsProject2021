2f16cb21a979c0e881679d7e5a04a060844ccefc
Maya O Riordan <maoriord@tcd.ie>
2021-04-08 13:32:17 +0100

    Update TST.java

--- TST.java
+++ TST.java
@@ -1,1 +1,8 @@
-
+"""
+  Searching for a bus stop by full name or by the first few characters in the name, 
+  using a ternary search tree (TST), returning the full stop information for each stop matching the search criteria (which can be zero, one or more stops) 
+  In order for this to provide meaningful search functionality please move keywords flagstop, wb, nb, sb, eb from start of the names to the end of the names
+  of the stops when reading the file into a TST (eg “WB HASTINGS ST FS HOLDOM AVE” becomes “HASTINGS ST FS HOLDOM AVE WB”)
+"""
+    
+   
