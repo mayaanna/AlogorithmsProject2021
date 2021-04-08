1f7124f6553f101847945f7eff913495d71aa79e
Maya O Riordan <maoriord@tcd.ie>
2021-04-08 13:35:08 +0100

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -1,1 +1,9 @@
-
+"""
+  Searching for all trips with a given arrival time,
+  returning full details of all trips matching the criteria (zero, one or more),
+  sorted by trip id Arrival time should be provided by the user as hh:mm:ss. 
+  When reading in stop_times.txt file you will need to remove all invalid times,
+  e.g., there are times in the file that start 27/28 hours, so are clearly invalid.
+  Maximum time allowed is 23:59:59
+"""
+  
