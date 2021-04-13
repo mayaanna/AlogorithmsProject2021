16f035f82e59d2ea50722c6cd5bf7224e2e9c895
cimorgan333 <81871444+cimorgan333@users.noreply.github.com>
2021-04-13 17:46:20 +0100

    Update Sorting.java

--- Sorting.java
+++ Sorting.java
@@ -6,4 +6,15 @@
   e.g., there are times in the file that start 27/28 hours, so are clearly invalid.
   Maximum time allowed is 23:59:59
 """
+    
+    
+    // ciara: what structure/ how do we want to store the stop_times.txts when we read them in 
+    
+    // Ciara Says: For sorting by trip ID:
+    // forgive me if im wrong but HO on quicksort or 3 way quick sort 
+    // has worst case space conplexity of nlogn
+    // average case time complexity is nlogN
+    
+    // Ciara: Radix sort seems like itll take too much memory & we're mot sure enough on range
+    //but again shot in dark from myself
   
