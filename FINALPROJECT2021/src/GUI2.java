5a23148182bd517f94861cb61288088b59017a0c
Maya O Riordan <maoriord@tcd.ie>
2021-04-18 11:16:20 +0100

    GUI using JFrame Template

---
+++ FINALPROJECT2021/src/GUI2.java
@@ -0,0 +1,95 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.AbstractAction;
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+
+public class GUI2 extends JFrame {
+
+	private JPanel contentPane;
+	private final Action action = new SwingAction();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					GUI2 frame = new GUI2();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public GUI2() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(400, 240, 650, 300);
+		
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Shortest Path Between 2 Bus Stops");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+				
+				
+			}
+		});
+		btnNewButton.setBounds(24, 85, 278, 47);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Exit");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				System.exit(0);
+			}
+		});
+		btnNewButton_1.setBounds(396, 180, 117, 56);
+		contentPane.add(btnNewButton_1);
+		
+		JLabel lblPleaseChooseThe = new JLabel("Please choose the program you wish to run or press exit:");
+		lblPleaseChooseThe.setBounds(40, 19, 378, 16);
+		contentPane.add(lblPleaseChooseThe);
+		
+		JButton btnNewButton_2 = new JButton("All Trips With A Given Arrival Time");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_2.setBounds(24, 176, 312, 64);
+		contentPane.add(btnNewButton_2);
+		
+		JButton btnNewButton_3 = new JButton("Stop Information Matching Search Criteria");
+		btnNewButton_3.setBounds(331, 85, 301, 47);
+		contentPane.add(btnNewButton_3);
+	}
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "SwingAction");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
+	}
+}
