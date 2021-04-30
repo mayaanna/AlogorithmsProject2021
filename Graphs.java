77066a08c56b98abee6fabbfd98f2675aa0e528e
Maya O Riordan <maoriord@tcd.ie>
2021-04-30 20:58:01 +0100

    Update Graphs.java

--- Graphs.java
+++ Graphs.java
@@ -10,7 +10,293 @@
   and for transfer type 2 the cost is the minimum transfer time divided by 100.
 """
 
-    public class ShortestPath {
-      
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ShortestPath 
+
+{
+
+	private double adjMatrix[][] = new double[12479][12479];
+	private String stop_times_txt; 
+	private String transfers_txt;
+
+
+
+
+
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+
+		printShortestPath();
+
+	}
+
+
+
+	public static void printShortestPath() throws FileNotFoundException
+	{
+		ShortestPath diagram = new ShortestPath("stop_times_txt", "transfers_txt");
+
+		try 
+
+		{
+			diagram.matrixBuilder();
+		}
+
+		catch (FileNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+
+		Scanner inputScanner = new Scanner (System.in);
+		System.out.print("Please enter source Bus stop");
+		int stopOne = inputScanner.nextInt();
+
+		System.out.print("Please enter the destination Bus stop");
+		int stopTwo = inputScanner.nextInt();
+
+		System.out.println("Shortest distance from " + stopOne + " to " + stopTwo + " is " + diagram.shortestRoute(stopOne, stopTwo));
+
+		inputScanner.close();
+
+
+	}
+
+
+
+	private void matrixBuilder() throws FileNotFoundException {
+
+		// 12479 being the max size of matrix - found by trial and error
+
+		for(int i = 0; i < 12479; i++)
+		{
+			for(int j = 0; j < 12479; j++) 
+			{
+				if(i != j)
+				{
+
+					adjMatrix[i][j] = Math.pow(1.7*10,308);
+
+				}
+				else {
+					adjMatrix[i][j] = 0;
+				}
+			}
+		}
+
+
+		int source = 0; 
+		int destination = 0; 
+		int lastTripId = 0;
+		int currentTripId = 0;
+		double cost =1;
+		double shortestTime;
+		int in_direct; 
+		String thisLine;
+
+
+		File stopTimesFile = new File(stop_times_txt);
+
+		Scanner completeFile = new Scanner(stopTimesFile);
+		Scanner lineFile = null;
+
+		completeFile.nextLine();
+
+
+		while(completeFile.hasNextLine()) 
+		{
+
+			thisLine = completeFile.nextLine();
+			lineFile = new Scanner(thisLine);
+			lineFile.useDelimiter(",");
+
+			lastTripId = currentTripId;
+			currentTripId = lineFile.nextInt();
+
+
+			for (int i = 1; i <3;i++)
+			{
+				lineFile.next();
+
+			}
+
+			source = destination;
+			destination = lineFile.nextInt();
+
+			if(lastTripId == currentTripId) 
+
+			{
+				adjMatrix[source][destination] = cost;
+
+			}
+
+			lineFile.close();
+		}
+
+		completeFile.close();
+
+
+
+		File transfersFile = new File(transfers_txt);
+		completeFile = new Scanner(transfersFile);
+
+
+		completeFile.nextLine();
+
+
+		while(completeFile.hasNextLine()) 
+		{
+
+			thisLine = completeFile.nextLine();
+			lineFile = new Scanner(thisLine);
+			lineFile.useDelimiter(",");
+
+			source = lineFile.nextInt();
+			destination = lineFile.nextInt();
+			in_direct = lineFile.nextInt();
+
+			if(in_direct == 0) 
+			{
+				adjMatrix[source][destination] = 2;
+			}
+			else if(in_direct == 2) 
+			{
+				shortestTime = lineFile.nextDouble();
+				adjMatrix[source][destination] = (shortestTime / 100 );
+			}
+
+			lineFile.close();
+		}
+		completeFile.close();
+	}
+
+
+
+
+
+
+
+	public ShortestPath(String stop_times_txt, String transfers_txt) throws FileNotFoundException 
+
+	{
+
+		this.stop_times_txt = stop_times_txt;
+		this.transfers_txt = transfers_txt;
+		matrixBuilder();
+	}
+
+
+
+
+	private void relaxEdge(int source, int destination, double[] distanceTo, int[] edgeTo) 
+	{
+		if(distanceTo[destination] > distanceTo[source] + adjMatrix[source][destination]) 
+		{
+			distanceTo[destination] = distanceTo[source] + adjMatrix[source][destination];
+			edgeTo[destination] = source;
+		}
+	}
+
+
+
+
+
+	public String shortestRoute(int source, int destination)
+
+	{
+
+
+
+		int marked[] = new int[12479];
+		double distanceTo[] = new double[12479];
+		int edgeTo[] = new int[12479];
+
+
+		marked[source] = 1;
+		distanceTo[source] = 0; 
+		int position = source;
+		int noStops = 0;
+		double shortestDist = Math.pow(1.7*10,308);
+
+
+		double node1 = source;
+		double node2 = destination;
+		int distLength = distanceTo.length;
+
+		String path = "";
+
+		for(int i = 0; i < distLength; i++) 
+		{
+			if(i != source)
+			{
+				distanceTo[i] = Double.POSITIVE_INFINITY;
+			}
+		}
+
+		if(destination  == source)
+		{
+			return "" + adjMatrix[source][destination] + " through " + source;
+		}
+
+
+
+
+		while(noStops < distLength)
+
+		{
+
+
+			for(int i = 0; i < distLength; i++) 
+			{
+				if(marked[i] != 1 && shortestDist > distanceTo[i]) 
+				{
+					position = i;
+					shortestDist = distanceTo[i];
+				}
+			}
+			noStops++;
+
+
+
+			for(int i = 0; i < adjMatrix[position].length; i ++) 
+			{
+
+				if(marked[i] == 0) 
+				{
+					relaxEdge(position, i, distanceTo, edgeTo);
+				}
+
+			}
+			marked[position] = 1;
+
+
+		}
+
+
+		if(distanceTo[destination] == Double.POSITIVE_INFINITY) 
+		{
+			return "There is no possible route.";
+		}
+
+
+
+
+		while(node2 != node1) 
+		{
+			path =  edgeTo[(int) node2] + path;
+			node2 = edgeTo[(int) node2];
+		}
+		path = path + "," +  destination;
+
+		return distanceTo[destination] + " through stops " + path;
+	}
+
+
+
+
+
+}
     
     }
